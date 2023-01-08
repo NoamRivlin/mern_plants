d5d85b8d2a30f2dc2c272fde9ac4173f8a7cfe2a
Noam Rivlin <noam.rivlin15@gmail.com>
2023-01-08 17:17:05 +0200

    [ft](config)added db file and created db on atlas

--- backend/controllers/plantController.js
+++ backend/controllers/plantController.js
@@ -2,39 +2,39 @@ const asyncHandler = require('express-async-handler');
 //route  GET /api/ideas
 //access  private
 
-const getPlants = async (req, res) => {
+const getPlants = asyncHandler(async (req, res) => {
   try {
     res.status(200).json({ message: 'get plants' });
   } catch (err) {
     console.log(err);
   }
-};
+});
 
 //route  POST /api/plants
 //access  private
 
-const createPlants = async (req, res) => {
+const createPlants = asyncHandler(async (req, res) => {
   console.log(req.body);
   if (!req.body.text) {
     res.status(400);
     throw new Error('add text field');
   }
   res.status(200).json({ message: 'create plants' });
-};
+});
 
 //route  PUT /api/ideas
 //access  private
 
-const updatePlant = async (req, res) => {
+const updatePlant = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `updated Plant ${req.params.id}` });
-};
+});
 
 //route  DELETE /api/Plants
 //access  private
 
-const deletePlant = async (req, res) => {
+const deletePlant = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `delete Plant ${req.params.id}` });
-};
+});
 
 module.exports = {
   getPlants,
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 4545;
 const { errHandler } = require('./middleware/errorMiddleware');
-
+const connectDB = require('./config/db');
+connectDB();
 const app = express();
 
 app.use(express.json());
