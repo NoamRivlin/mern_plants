e35510cf656e135265b91258b4250ca4814679ac
Noam Rivlin <noam.rivlin15@gmail.com>
2023-01-19 17:42:10 +0200

    [feat](features,pages)making components and starting react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 // import React from 'react';
+import { BrowserRouter as Router, Routers, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div>
-      <h1>sup</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Routers>
+            <Route path='/' element={<Dashboard />}></Route>
+          </Routers>
+        </div>
+      </Router>
+    </>
   );
 }
 
